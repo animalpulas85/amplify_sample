d6122a6abfbd7cc396db5ce4da1189838f754f17
Miyazaki Yasufumi <33385996+animalpulas85@users.noreply.github.com>
2023-03-10 13:08:05 +0900

    aaaaUpdate App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   <div className="App">
    <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
-    <p>This is Hello World.</p>
+    <p>This is Hello World-V2.</p>
     <a
      className="App-link"
      href="https://reactjs.org"
